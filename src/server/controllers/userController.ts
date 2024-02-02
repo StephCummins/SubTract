4e3e70115a22eb85986908f2e1a0d6a30c0c157e
StephCummins <steph.a.cummins@gmail.com>
2024-02-02 13:33:19 -0800

    Added pashword hashing and user authentication on login with bcrypt

--- src/server/controllers/userController.ts
+++ src/server/controllers/userController.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
 import db from '../database/db';
 import type ErrorMessage from '../models/errorInterface';
 
 interface NewUserController {
   addNewUser(req: Request, res: Response, next: NextFunction): any;
   login(req: Request, res: Response, next: NextFunction): any;
+  hashPassword(req: Request, res: Response, next: NextFunction): any;
+  authUser(req: Request, res: Response, next: NextFunction): any;
 }
 
 const userController: NewUserController = {
@@ -13,6 +16,7 @@ const userController: NewUserController = {
       const user = req.body!;
       console.log('Entered User Controller!');
       console.log(req.body);
+      console.log(res.locals.password);
 
       const newUserData = `INSERT INTO users (first_name, last_name, email, password, google_auth, picture) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`;
 
@@ -20,7 +24,7 @@ const userController: NewUserController = {
         user.firstName,
         user.lastName,
         user.email,
-        user.password,
+        res.locals.password,
         user.googleAuth,
         user.picture
       ];
@@ -40,12 +44,13 @@ const userController: NewUserController = {
 
   async login(req, res, next) {
     try {
-      const { password } = req.body;
+      const { email } = req.body;
 
-      const loginData = `SELECT * FROM users WHERE password = $1`;
-      const queryParam = [password];
+      const loginData = `SELECT * FROM users WHERE email = $1`;
+      const queryParam = [email];
 
       const response: any = await db.query(loginData, queryParam);
+
       res.locals.userData = response.rows[0];
       return next();
     } catch (error) {
@@ -53,6 +58,46 @@ const userController: NewUserController = {
       const message: ErrorMessage = {
         log: 'Error at userController.login',
         message: { error: 'Error logging in' }
+      };
+      return next(message);
+    }
+  },
+
+  hashPassword(req, res, next) {
+    try {
+      const { password } = req.body;
+      const saltRounds = 10;
+
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) throw new Error('bcrypt hashing error');
+        else {
+          res.locals.password = hash;
+          return next();
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      const message: ErrorMessage = {
+        log: 'Error at userController.hashPassword',
+        message: { error: 'Error hashing user password' }
+      };
+      return next(message);
+    }
+  },
+
+  async authUser(req, res, next) {
+    try {
+      const { password } = req.body;
+      const result = await bcrypt.compare(
+        password,
+        res.locals.userData.password
+      );
+      if (result) throw new Error('Invalid login credentials!');
+      else return next();
+    } catch (error) {
+      const message: ErrorMessage = {
+        log: 'Error at userController.authUser',
+        message: { error: 'Error authenticating user' }
       };
       return next(message);
     }
--- src/server/routes/userRouter.ts
+++ src/server/routes/userRouter.ts
@@ -4,6 +4,7 @@ const userRouter: Router = express.Router();
 
 userRouter.post(
   '/signup',
+  userController.hashPassword,
   userController.addNewUser,
   (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).send(res.locals.newUser);
@@ -13,6 +14,7 @@ userRouter.post(
 userRouter.post(
   '/login',
   userController.login,
+  userController.authUser,
   (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).send(res.locals.userData);
   }

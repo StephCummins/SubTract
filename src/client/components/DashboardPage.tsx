153845f67e73ba5c6d9993c4511c34d71cbff45a
StephCummins <steph.a.cummins@gmail.com>
2024-02-03 17:18:01 -0800

    Set up Dashboard CSS with MaterialUI

--- src/client/components/DashboardPage.tsx
+++ src/client/components/DashboardPage.tsx
@@ -1,11 +1,73 @@
 import React from 'react';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
 
 const DashboardPage = ({ user, setUser }): JSX.Element => {
   console.log('In Dashboard Page!', user);
-  /*
-  On page load, get all of a users subscriptions and load them into some sort of visual thingy!
-  */
-  return <h1>Dashboard Page!</h1>;
+
+  function createData(
+    id: number,
+    date: string,
+    name: string,
+    shipTo: string,
+    paymentMethod: string,
+    amount: number
+  ) {
+    return { id, date, name, shipTo, paymentMethod, amount };
+  }
+
+  function preventDefault(event: React.MouseEvent) {
+    event.preventDefault();
+  }
+
+  const rows = [
+    createData(
+      0,
+      '16 Mar, 2019',
+      'Elvis Presley',
+      'Tupelo, MS',
+      'VISA ⠀•••• 3719',
+      312.44
+    )
+  ];
+
+  return (
+    <React.Fragment>
+      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+        Subscriptions
+      </Typography>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Ship To</TableCell>
+            <TableCell>Payment Method</TableCell>
+            <TableCell align="right">Sale Amount</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.date}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.shipTo}</TableCell>
+              <TableCell>{row.paymentMethod}</TableCell>
+              <TableCell align="right">{`$${row.amount}`}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+        See more orders
+      </Link>
+    </React.Fragment>
+  );
 };
 
 export default DashboardPage;
